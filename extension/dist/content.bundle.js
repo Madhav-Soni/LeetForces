94a322668ca839989fd78d95abede3f809932e4a
Madhav <madhavAO321F@gmail.com>
2026-09-11 19:23:15 +0530

    refactor: remove manual node.attributes synchronization in DOM element creation logic

--- extension/dist/content.bundle.js
+++ extension/dist/content.bundle.js
@@ -1664,13 +1664,11 @@ function el(doc, tag, attrs = {}, text) {
             else node.style = { cssText: css };
         } else if (key === 'id') {
             node.id = value;
-            if (node.attributes) node.attributes.id = value;
             if (typeof node.setAttribute === 'function') node.setAttribute('id', value);
         } else if (typeof node.setAttribute === 'function') {
             node.setAttribute(key, value);
         } else {
             node[key] = value;
-            if (node.attributes) node.attributes[key] = value;
         }
     }
     if (text !== undefined) node.textContent = text;
